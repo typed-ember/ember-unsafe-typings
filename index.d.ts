88a9aaf53d5eb843e11b37df6c1db1337f745191
Derek Wickern <dwickern@gmail.com>
2017-09-21 18:17:21 -0700

    initial commit

---
+++ index.d.ts
@@ -0,0 +1,29 @@
+declare module 'ember' {
+    namespace Ember {
+        function get(obj: any, key: string): any;
+        function set(obj: any, key: string, value: any): any;
+        function getProperties<K extends string>(obj: any, keys: K[]): Pick<any, K>;
+        function getProperties<K extends string>(obj: any, ...keys: K[]): Pick<any, K>;
+        function setProperties<K extends string>(obj: any, hash: Pick<any, K>): Pick<any, K>;
+
+        interface CoreObject {
+            get(key: string): any;
+            set(key: string, value: any): any;
+            getProperties<K extends string>(keys: K[]): Pick<any, K>;
+            getProperties<K extends string>(...keys: K[]): Pick<any, K>;
+            setProperties<K extends string>(hash: Pick<any, K>): Pick<any, K>;
+        }
+
+        interface Array<T> {
+            getEach(key: string): any[];
+            setEach(key: string, value: any): any;
+            mapBy(key: string): any[];
+            filterBy(key: string, value?: any): T[];
+            rejectBy(key: string, value?: any): T[];
+            findBy(key: string, value: any): T | undefined;
+            isEvery(key: string, value?: boolean): boolean;
+            isAny(key: string, value?: boolean): boolean;
+            sortBy(...property: string[]): T[];
+        }
+    }
+}
